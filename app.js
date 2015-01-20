9c336d7622430ec8df38c0b778eb531e4194eada
Chinedu Amadi-Ndukwe <ndukwec@uni.coventry.ac.uk>
2015-01-20 17:22:37 +0000

    Added Mongo DB

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+
+mongoose.connect('mongodb://heroku_app33373738:k0cja96r943p0h5p5rdbjok3sn@ds033831.mongolab.com:33831/heroku_app33373738');
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-    response.send('Hello World!');
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+
+var User = mongoose.model('User', {
+    email: String,
+    password: String
+});
+
+
+app.post('/users', function(req, res){
+    console.log("Params: " + req.body.email + "");
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    console.log("About to save user");
+    user.save(function (err, newUser) {
+        if (err)
+            return console.error(err);
+        res.send({
+            message: "User successfully added"
+        });
+    });
 });
 
 app.listen(app.get('port'), function() {
