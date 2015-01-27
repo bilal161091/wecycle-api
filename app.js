2967fc63d46f480269723686f07eeb4dbae6f015
Chinedu Amadi-Ndukwe <ndukwec@uni.coventry.ac.uk>
2015-01-27 10:52:05 +0000

    removed model folder, added restify.bodyparser to get posts working

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var app = restify.createServer();
 var mongoose = require('mongoose');
 var http = require('http');
 
+
 app
     .use(restify.fullResponse())
     .use(restify.bodyParser());
 
+//connection to mongodb
 mongoose.connect('mongodb://heroku_app33373738:k0cja96r943p0h5p5rdbjok3sn@ds033831.mongolab.com:33831/heroku_app33373738');
 
 app.get('/', function(req, res) {
