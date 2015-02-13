aa65d5d013c2c5c0c2f145a3ecab0b25414d6eef
Chinedu Amadi-Ndukwe <ndukwec@uni.coventry.ac.uk>
2015-02-13 17:44:56 +0000

    restructured API, added url to items model so user can post url links of pictures of items they posted to the database

--- app.js
+++ app.js
@@ -11,16 +11,27 @@ app
 //connection to mongodb
 mongoose.connect('mongodb://heroku_app33373738:k0cja96r943p0h5p5rdbjok3sn@ds033831.mongolab.com:33831/heroku_app33373738');
 
+
+var User = mongoose.model('User', {
+    email: String,
+    password: String,
+    phone_number: String,
+    name: String
+});
+
+var Item = mongoose.model('Item', {
+    name: String,
+    description: String,
+    url: [String]
+});
+
+
 app.get('/', function(req, res) {
     res.send('default route');
 });
 
-app.get('/items', function(req, res) {
-    var query = Item.where({});
-    query.find(function (err, items) {
-        res.send(items);
-    });
-});
+
+
 
 app.get('/users', function(req, res){
     var query = User.where({});
@@ -29,25 +40,21 @@ app.get('/users', function(req, res){
     });
 });
 
-var User = mongoose.model('User', {
-    email: String,
-    password: String
+app.get('/users/:user_id', function(req, res){
+    var query = User.where({_id: req.params.user_id});
+    query.find(function (err, users){
+        res.send(users);
+    });
 });
-
-var Item = mongoose.model('Item', {
-    name: String,
-    description: String
-});
-
-
-
 
 //post users
 app.post('/users', function(req, res){
     console.log("Params: " + req.params.email + "");
     var user = new User({
         email: req.params.email,
-        password: req.params.password
+        password: req.params.password,
+        phone_number: req.params.phone_number,
+        name: req.params.name
     });
 
     console.log("About to save user");
@@ -60,15 +67,34 @@ app.post('/users', function(req, res){
     });
 });
 
+
+
+
+
+app.get('/items', function(req, res) {
+    var query = Item.where({});
+    query.find(function (err, items) {
+        res.send(items);
+    });
+});
+
+app.get('/items/:item_id', function(req, res){
+    var query = Item.where({_id: req.params.item_id});
+    query.find(function (err, items){
+        res.send(items);
+    });
+});
+
 //post items
 app.post('/items', function(req, res){
-    console.log("Params: " + req.params.name + "");
+
     var item = new Item({
         name: req.params.name,
-        description: req.params.description
+        description: req.params.description,
+        url: req.params.url.split(", ")
     });
 
-    console.log("About to save item");
+
     item.save(function (err, newItem) {
         if (err)
             return console.error(err);
@@ -77,6 +103,7 @@ app.post('/items', function(req, res){
         });
     });
 });
+
 
 //listening @ localhost port
 app.listen((process.env.PORT || 5000), function() {
